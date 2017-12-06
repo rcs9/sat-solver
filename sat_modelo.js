d8b03f2b645f2ff7def87556920a8caa31d51e51
rcs9 <rcs9@cin.ufpe.br>
2017-12-06 20:07:11 -0300

    Final commit before deadline :(

--- sat_modelo.js
+++ sat_modelo.js
@@ -1,27 +1,27 @@
 /**
- * This file should be placed at the node_modules sub-directory of the directory where you're 
+ * This file should be placed at the node_modules sub-directory of the directory where you're
  * executing it.
- * 
- * Written by Fernando Castor in November/2017. 
+ *
+ * Written by Fernando Castor in November/2017.
  */
 exports.solve = function(fileName) {
-  let formula = propsat.readFormula(fileName)
+  let formula = readFormula(fileName)
   let result = doSolve(formula.clauses, formula.variables)
   return result // two fields: isSat and satisfyingAssignment
 }
 
 // Receives the current assignment and produces the next one
 function nextAssignment(currentAssignment) {
-  // implement here the code to produce the next assignment based on currentAssignment. 
+  // implement here the code to produce the next assignment based on currentAssignment.
   return newAssignment
 }
 
 function doSolve(clauses, assignment) {
   let isSat = false
   while ((!isSat) && /* must check whether this is the last assignment or not*/) {
-    // does this assignment satisfy the formula? If so, make isSat true. 
+    // does this assignment satisfy the formula? If so, make isSat true.
 
-    // if not, get the next assignment and try again. 
+    // if not, get the next assignment and try again.
     assignment = nextAssignment(assignment)
   }
   let result = {'isSat': isSat, satisfyingAssignment: null}
@@ -30,15 +30,19 @@ function doSolve(clauses, assignment) {
   }
   return result
 }
-  
+
 function readFormula(fileName) {
-  // To read the file, it is possible to use the 'fs' module. 
-  // Use function readFileSync and not readFile. 
+  // To read the file, it is possible to use the 'fs' module.
+  // Use function readFileSync and not readFile.
   // First read the lines of text of the file and only afterward use the auxiliary functions.
-  let text // = ...  //  an array containing lines of text extracted from the file. 
+  let fs = require('fs')
+  fileName = fs.readFileSync(fileName) //let fileName = 'fileName.cnf'?
+  let contents = fileName.toString();
+  let text = new Array(contents.) // An array containing lines of text extracted from the file.
+
   let clauses = readClauses(text)
   let variables = readVariables(clauses)
-  
+
   // In the following line, text is passed as an argument so that the function
   // is able to extract the problem specification.
   let specOk = checkProblemSpecification(text, clauses, variables)
@@ -49,4 +53,15 @@ function readFormula(fileName) {
     result.variables = variables
   }
   return result
+}
+
+function readClauses(text) {
+
+}
+
+function totalClauses(text) {
+  let index = text.indexOf('cnf')
+  text = text.substring(index)
+  let line = text.substring(text.charAt(4), text.indexOf('\n'))
+  return line.substring(0, line.indexOf(' '))
 }
--- test.js
+++ test.js
@@ -1,9 +1,28 @@
 //Learning syntax *sigh*
 let fs = require('fs');
+let file = fs.readFileSync('simple0.cnf');
+let text = file.toString();
 
-function readFormula(cnfFile) {
-  let cnf {
-    clauses;
-    variables;
-  }
+function totalClauses(text) {
+  let index = text.indexOf('cnf')
+  text = text.substring(index)
+  let line = text.substring(text.charAt(4), text.indexOf('\n'))
+  return line.substring(0, line.indexOf(' '))
 }
+
+function toArray(text, clauses) {
+  let lines = []
+  let i = 0
+  while(i < 10/*text.length*/) {
+    lines.push(text.substring(0, text.indexOf('\n')))
+    text = text.substring(text.indexOf('\n'))
+    //console.log(text.substring(text.indexOf('\n')))
+    i++
+    //i = i + text.indexOf('\n') //text.indexOf('\n')
+    //text = text.substring(i)
+  }
+  return lines
+}
+
+let clauses = totalClauses(text)
+console.log(toArray(text, clauses))
